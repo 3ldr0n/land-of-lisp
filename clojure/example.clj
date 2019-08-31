8f2956d4322384e46c837332b15187d3d6e584ec
Edison Neto <ednetoali@gmail.com>
2019-08-31 12:28:28 -0300

    Update clojure example

--- clojure/example.clj
+++ clojure/example.clj
@@ -1,11 +1,3 @@
-;; Maps
-(def scores {"Fred" 1400
-             "Bob" 1240
-             "Angelo" 2013})
-
-(println scores)
-
-
 ;; Literals
 
 ; Negative infinity
@@ -16,3 +8,32 @@
 ; Strings
 (def my-name "Fred Jones")
 (println my-name)
+
+; Collections
+; list
+(def random-values '(6 3 84 1 39))
+(println random-values)
+
+; vector
+(def random-values-vector [9 2 47 13 6])
+(println random-values-vector)
+
+; set
+(def random-set #{3 54 6 887 1})
+(println random-set)
+
+; map
+(def my-map
+  {:a 2,
+   :b 5,
+   :c 4,
+   :d 8})
+(println my-map)
+
+;; Maps
+(def scores {"Fred" 1400
+             "Bob" 1240
+             "Angelo" 2013})
+
+(println scores)
+

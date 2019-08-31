bea706006534734f556715e5502afe459d338a31
Edison Neto <ednetoali@gmail.com>
2019-08-31 12:20:14 -0300

    Clojure example

---
+++ clojure/example.clj
@@ -0,0 +1,18 @@
+;; Maps
+(def scores {"Fred" 1400
+             "Bob" 1240
+             "Angelo" 2013})
+
+(println scores)
+
+
+;; Literals
+
+; Negative infinity
+(println ##-Inf)
+; Positive infinity
+(println ##Inf)
+
+; Strings
+(def my-name "Fred Jones")
+(println my-name)
